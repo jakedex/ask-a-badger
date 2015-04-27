d8cc9a67d67b2cf120479dfad390a3eef068e9d0
Jake Dexheimer <jakedex@gmail.com>
2015-04-26 23:14:01 -0500

    single quotes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 
 function changeSubmitOnFocus() {
-    document.getElementById('submit').style.color = "#e6e8eb";
+    document.getElementById('submit').style.color = '#e6e8eb';
 }
 
 function changeSubmitOnBlur() {
-    document.getElementById('submit').style.color = "#600700";
+    document.getElementById('submit').style.color = '#600700';
 }
 
 document.getElementById('tel').onfocus = changeSubmitOnFocus;
