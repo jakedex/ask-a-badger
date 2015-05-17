df24204861dfdfdaf9f879b17708de358b793f87
Jake Dexheimer <jakedex@gmail.com>
2015-05-17 18:02:19 -0500

    added sending animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,56 @@
 'use strict';
 
-function changeSubmitOnFocus() {
-    document.getElementById('submit').style.color = '#e6e8eb';
-}
-
-function changeSubmitOnBlur() {
-    document.getElementById('submit').style.color = '#600700';
-}
-
-document.getElementById('tel').onfocus = changeSubmitOnFocus;
-document.getElementById('tel').onblur = changeSubmitOnBlur;
+$(function() {
+    var $submit = $('#submit');
+    var $tel = $('#tel');
+    
+    //change "text me" on focus
+    $tel.on('focus', function() {
+       $submit.css('color', '#e6e8eb');
+    });
+    
+    $tel.on('blur', function() {
+        //$submit.css('color', '#600700');
+        
+        $submit.fadeOut(500);
+        $tel.fadeOut(500);
+            
+        //progress bar
+        $('.sending').delay(500).fadeIn(500, function() {
+            var percent = '';
+            for (var i = 0; i<= 100; i++) {
+                percent = i + '%';
+                $('.progress-bar').css('width', percent);
+            }
+        });
+    });
+    
+    //post on submit
+    $tel.on('submit', function(e) {
+        e.preventDefault();
+        var num = $tel.serialize();
+        
+        $submit.fadeOut(500);
+        $tel.fadeOut(500);
+            
+        //progress bar
+        $('.sending').delay(500).fadeIn(500, function() {
+            var percent = '';
+            for (var i = 0; i<= 100; i++) {
+                percent = i + '%';
+                $('.progress-bar').css('width', percent);
+            }
+        });
+        
+        //use $.ajax??
+        $.post('sendnotifications.php', num, function(data) {
+            
+            
+            //fix to chnage response message
+            $('#response').html(data);
+        }).fail( function() {
+            $('#response').html('<p>Sending Failed. Please try again later.</p>');
+        });
+        
+    });
+});
