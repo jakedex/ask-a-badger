ed393703daa2b11622a04962aab8bff36054ce9e
Jake Dexheimer <jakedex@gmail.com>
2016-02-20 16:13:23 -0600

    Preusers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220110124) do
+ActiveRecord::Schema.define(version: 20160220200322) do
+
+  create_table "preusers", force: :cascade do |t|
+    t.integer  "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "preusers", ["phone"], name: "index_preusers_on_phone", unique: true
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
