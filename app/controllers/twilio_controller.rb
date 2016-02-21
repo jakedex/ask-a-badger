f4f8aabf5bf3c8a2f2596f45793563d54ab296a3
Jake Dexheimer <jakedex@gmail.com>
2016-02-21 15:14:03 -0600

    Without send meth

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -10,15 +10,6 @@ class TwilioController < ApplicationController
     @initial_msg = "Simply reply in the following format to get started.\n\nFormat: course_number question\n(E.g. CS368 How do pointers work in c++?)"
   end
 
-  def send data
-    # @client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
-    client = Twilio::REST::Client.new "ACe01140862912970761c0a7db87f0b6d4", "5807030bb9cebf8d8033f1031e03d96c"
-    num_to = params[:num] ? params[:num] : (params[:From][1..-1]).to_i
-
-    message = client.messages.create from: '16084674004', to: num_to, body: "Welcome to Ask A Badger. " + @initial_msg
-    render plain: message.status
-  end
-
   def reply
     from = params[:From][1..-1]
     body = params[:Body]
@@ -59,6 +50,15 @@ class TwilioController < ApplicationController
 
     render text: response.text
   end
+
+  # def send data
+  #   # @client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
+  #   client = Twilio::REST::Client.new "ACe01140862912970761c0a7db87f0b6d4", "5807030bb9cebf8d8033f1031e03d96c"
+  #   num_to = params[:num] ? params[:num] : (params[:From][1..-1]).to_i
+
+  #   message = client.messages.create from: '16084674004', to: num_to, body: "Welcome to Ask A Badger. " + @initial_msg
+  #   render plain: message.status
+  # end
 
   def status
     # the status can be found in params['MessageStatus']
