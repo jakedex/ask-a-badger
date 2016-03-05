70ea33674788cc665094b214f217de1296b6d762
Jake Dexheimer <jakedex@gmail.com>
2016-03-04 18:58:17 -0600

    Index changes for un/answered, show view answer changes, answer form css

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,6 +1,4 @@
 $(function () {
-  console.log('running');
-
   // show trix ele
   var show_answer_btn = $('.show-answer-btn');
   show_answer_btn.click(function (event){
@@ -10,42 +8,36 @@ $(function () {
     $('form#new_answer').show();
   });
 
-  $('#answer_btn').click(function (event) { //Catch form submit event
-    console.log('caught');
-//    event.preventDefault(); //Stop it from posting back unless you tell it to
-    var everythingIsOkay = true;
+  $('#answer_btn').click(function (event) {
 
-    if (everythingIsOkay) {
+    // get editor and it's document
+    var trix = document.querySelector('trix-editor.formatted_content');
+    console.log(trix);
+    console.log(trix.editor);
 
-      // get editor and it's document
-      var trix = document.querySelector('trix-editor.formatted_content');
-      console.log(trix);
-      console.log(trix.editor);
+    // get value of response method radio
+    var isText = $("#new_answer input[type='radio']:checked").val();
+    console.log(isText);
 
-      // get value of response method radio
-      var isText = $("#new_answer input[type='radio']:checked").val();
-      console.log(isText);
+    // strip formatting
+    var plain = trix.editor.getDocument().toString();
 
-      // strip formatting
-      var plain = trix.editor.getDocument().toString();
+    console.log(plain);
 
-      console.log(plain);
+    // add body_plain param, post
+    var form_ele = $("#new_answer");
+    var form = form_ele.serializeArray();
+    form = form.concat({
+      name: "body_plain",
+      value: plain
+    });
 
-      // add body_plain param, post
-      var form_ele = $("#new_answer");
-      var form = form_ele.serializeArray();
-      form = form.concat({
-        name: "body_plain",
-        value: plain
-      });
+    console.log(form);
 
-      console.log(form);
-
-      $.post(form_ele.action, form, function (d) {
-        if (d.error) {
-          alert("There was a problem updating your user details");
-        }
-      });
-    }
+    $.post(form_ele.action, form, function (d) {
+      if (d.error) {
+        alert("There was a problem updating your user details");
+      }
+    });
   });
 });
